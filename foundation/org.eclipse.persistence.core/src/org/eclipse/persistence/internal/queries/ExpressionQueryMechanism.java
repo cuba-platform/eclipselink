9337f95238c255eb88c6b822d425014eb1176aed
Konstantin Krivopustov <krivopustov@haulmont.com>
2016-01-18 12:16:07 +0400

    PL-6448 Comparing an attribute value with null works in JPQL select, but does not in JPQL update and delete

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/queries/ExpressionQueryMechanism.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/queries/ExpressionQueryMechanism.java
@@ -1223,7 +1223,9 @@ public class ExpressionQueryMechanism extends StatementQueryMechanism {
             }
 
             SQLSelectStatement selectStatementForExist = createSQLSelectStatementForModifyAll(whereClause);
-
+            // cuba: needed for comparing attributes to null
+            selectStatementForExist.setTranslationRow(getQuery().getTranslationRow());
+            // cuba end
             // Main Case: Descriptor is mapped to more than one table and/or the query references other tables
             boolean isMainCase = selectStatementForExist.requiresAliases();
             if (isMainCase) {
@@ -1989,7 +1991,9 @@ public class ExpressionQueryMechanism extends StatementQueryMechanism {
 
         SQLCall selectCallForExist = null;
         SQLSelectStatement selectStatementForExist = createSQLSelectStatementForModifyAll(getSelectionCriteria());
-
+        // cuba: needed for comparing attributes to null
+        selectStatementForExist.setTranslationRow(getQuery().getTranslationRow());
+        // cuba end
         // Main Case: Descriptor is mapped to more than one table and/or the query references other tables
         boolean isMainCase = selectStatementForExist.requiresAliases();
         if(isMainCase) {
