0a032f5c91c9e9723031ad69bb414c36781a1647
Konstantin Krivopustov <krivopustov@haulmont.com>
2016-04-15 19:21:48 +0400

    CUBA: Prevent fetch of lazy fields when transaction is already finished

--- jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/EntityManagerImpl.java
+++ jpa/org.eclipse.persistence.jpa/src/org/eclipse/persistence/internal/jpa/EntityManagerImpl.java
@@ -2993,7 +2993,7 @@ public class EntityManagerImpl implements org.eclipse.persistence.jpa.JpaEntityM
     public static void processUnfetchedAttribute(FetchGroupTracker entity, String attributeName) {
         String errorMsg = entity._persistence_getFetchGroup().onUnfetchedAttribute(entity, attributeName);
         if(errorMsg != null) {
-            throw new javax.persistence.EntityNotFoundException(errorMsg);
+            throw new IllegalStateException(errorMsg); // cuba: changed to IllegalStateException
         }
     }
 
@@ -3004,7 +3004,7 @@ public class EntityManagerImpl implements org.eclipse.persistence.jpa.JpaEntityM
     public static void processUnfetchedAttributeForSet(FetchGroupTracker entity, String attributeName) {
         String errorMsg = entity._persistence_getFetchGroup().onUnfetchedAttributeForSet(entity, attributeName);
         if(errorMsg != null) {
-            throw new javax.persistence.EntityNotFoundException(errorMsg);
+            throw new IllegalStateException(errorMsg); // cuba: changed to IllegalStateException
         }
     }
 
