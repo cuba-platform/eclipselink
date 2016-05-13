070753fd54889ff96d81d485b2dd4a7037f00e02
Andrey Subbotin <subbotin@haulmont.com>
2016-05-13 10:59:17 +0400

    PL-7039 Sorting table by column affects usage of inner or left outer join

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/jpa/jpql/ExpressionBuilderVisitor.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/jpa/jpql/ExpressionBuilderVisitor.java
@@ -171,7 +171,15 @@ final class ExpressionBuilderVisitor implements EclipseLinkExpressionVisitor {
      */
     private boolean nullAllowed;
 
-    /**
+    //cuba start
+	/**
+	 * Determines whether the target relationship is allowed to be <code>null</code> in sort by condition.
+	 */
+	private boolean nullAllowedInSortBy;
+	//cuba end
+
+
+	/**
      * This {@link Comparator} compares two {@link Class} values and returned the appropriate numeric
      * type that takes precedence.
      */
@@ -1627,8 +1635,13 @@ final class ExpressionBuilderVisitor implements EclipseLinkExpressionVisitor {
     @Override
     public void visit(OrderByItem expression) {
 
-        // Create the item
-        expression.getExpression().accept(this);
+		// Create the item
+		//cuba start
+		try {
+			nullAllowedInSortBy = true;expression.getExpression().accept(this);} finally {
+			nullAllowedInSortBy = false;
+		}
+		//cuba end
 
         // Create the ordering item
         switch (expression.getOrdering()) {
@@ -2130,6 +2143,7 @@ final class ExpressionBuilderVisitor implements EclipseLinkExpressionVisitor {
         resolver.checkMappingType = false;
         resolver.localExpression  = null;
         resolver.descriptor       = null;
+		resolver.nullAllowedInSortBy = nullAllowedInSortBy;
 
         expression.accept(resolver);
 
@@ -2482,7 +2496,14 @@ final class ExpressionBuilderVisitor implements EclipseLinkExpressionVisitor {
          */
         boolean nullAllowed;
 
-        /**
+        //cuba start
+		/**
+		 * Determines whether the target relationship is allowed to be <code>null</code> in sort by condition.
+		 */
+		boolean nullAllowedInSortBy;
+		//cuba end
+
+		/**
          * Resolves a database column.
          *
          * @param expression The path expression representing an identification variable mapping to a
@@ -2531,6 +2552,9 @@ final class ExpressionBuilderVisitor implements EclipseLinkExpressionVisitor {
                 DatabaseMapping mapping = descriptor.getObjectBuilder().getMappingForAttributeName(path);
                 boolean last = (index + 1 == count);
                 boolean collectionMapping = false;
+				//cuba start
+				boolean foreignReferenceMapping = false;
+				//cuba end
 
                 // The path is a mapping
                 if (mapping != null) {
@@ -2542,6 +2566,9 @@ final class ExpressionBuilderVisitor implements EclipseLinkExpressionVisitor {
 
                     // This will tell us how to create the Expression
                     collectionMapping = mapping.isCollectionMapping();
+					//cuba start
+					foreignReferenceMapping = mapping.isForeignReferenceMapping();
+					//cuba end
 
                     // Retrieve the reference descriptor so we can continue traversing the path
                     if (!last) {
@@ -2564,7 +2591,9 @@ final class ExpressionBuilderVisitor implements EclipseLinkExpressionVisitor {
 
                         // This will tell us how to create the Expression
                         collectionMapping = queryKey.isCollectionQueryKey();
-
+						//cuba start
+						foreignReferenceMapping = queryKey.isForeignReferenceQueryKey();
+						//cuba end
                         // Retrieve the reference descriptor so we can continue traversing the path
                         if (!last && queryKey.isForeignReferenceQueryKey()) {
                             ForeignReferenceQueryKey referenceQueryKey = (ForeignReferenceQueryKey) queryKey;
@@ -2589,7 +2618,9 @@ final class ExpressionBuilderVisitor implements EclipseLinkExpressionVisitor {
                 else {
                     if (last && nullAllowed) {
                         localExpression = localExpression.getAllowingNull(path);
-                    }
+                    } else if (!last && foreignReferenceMapping && nullAllowedInSortBy) { //cuba start
+						localExpression = localExpression.getAllowingNull(path);
+					} //cuba end
                     else {
                         localExpression = localExpression.get(path);
                     }
