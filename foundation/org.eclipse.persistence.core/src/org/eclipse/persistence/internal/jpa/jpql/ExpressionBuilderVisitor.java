6fa1afb1889e556e8bf68cde0a90f2e141a67b81
Andrey Subbotin <subbotin@haulmont.com>
2016-05-23 12:48:22 +0400

    PL-7039 Sorting table by column affects usage of inner or left outer join (sorting by entity without instance name)

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/jpa/jpql/ExpressionBuilderVisitor.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/jpa/jpql/ExpressionBuilderVisitor.java
@@ -186,6 +186,11 @@ final class ExpressionBuilderVisitor implements EclipseLinkExpressionVisitor {
 	 * Determines whether the target relationship is allowed to be <code>null</code> in sort by condition.
 	 */
 	private boolean nullAllowedInSortBy;
+
+	/**
+	 * Determines whether the target expression 'is null' expression
+	 */
+	boolean isNullExpression;
 	//cuba end
 
 
@@ -1565,27 +1570,35 @@ final class ExpressionBuilderVisitor implements EclipseLinkExpressionVisitor {
         type[0] = Boolean.class;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void visit(NullComparisonExpression expression) {
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void visit(NullComparisonExpression expression) {
+//cuba begin
+		if (!expression.hasNot()) {
+			isNullExpression = true;
+		}
+		//cuba end
+		try {
+		// Create the expression first
+		expression.getExpression().accept(this);
 
-        // Create the expression first
-        expression.getExpression().accept(this);
+		// Mark it as NOT NULL
+		if (expression.hasNot()) {
+			queryExpression = queryExpression.notNull();
+		}
+		// Mark it as IS NULL
+		else {
+			queryExpression = queryExpression.isNull();
+		}
 
-        // Mark it as NOT NULL
-        if (expression.hasNot()) {
-            queryExpression = queryExpression.notNull();
-        }
-        // Mark it as IS NULL
-        else {
-            queryExpression = queryExpression.isNull();
-        }
-
-        // Set the expression type
-        type[0] = Boolean.class;
-    }
+		// Set the expression type
+		type[0] = Boolean.class;
+	}finally {
+			isNullExpression = false;
+		}
+	}
 
     /**
      * {@inheritDoc}
@@ -2181,6 +2194,7 @@ final class ExpressionBuilderVisitor implements EclipseLinkExpressionVisitor {
         resolver.localExpression  = null;
         resolver.descriptor       = null;
 		resolver.nullAllowedInSortBy = nullAllowedInSortBy;
+		resolver.isNullExpression = isNullExpression;
 
         expression.accept(resolver);
 
@@ -2538,6 +2552,11 @@ final class ExpressionBuilderVisitor implements EclipseLinkExpressionVisitor {
 		 * Determines whether the target relationship is allowed to be <code>null</code> in sort by condition.
 		 */
 		boolean nullAllowedInSortBy;
+
+		/**
+		 * Determines whether the target expression 'is null' expression
+		 */
+		boolean isNullExpression;
 		//cuba end
 
 		/**
@@ -2656,6 +2675,8 @@ final class ExpressionBuilderVisitor implements EclipseLinkExpressionVisitor {
                     if (last && nullAllowed) {
                         localExpression = localExpression.getAllowingNull(path);
                     } else if (foreignReferenceMapping && nullAllowedInSortBy) { //cuba start
+						localExpression = localExpression.getAllowingNull(path);
+					} else if (last && foreignReferenceMapping && isNullExpression) {
 						localExpression = localExpression.getAllowingNull(path);
 					} //cuba end
                     else {
