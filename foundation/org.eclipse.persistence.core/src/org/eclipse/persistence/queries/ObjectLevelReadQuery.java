39b90b06158e27918244430fee239cede62c7ea6
Andrey Subbotin <subbotin@haulmont.com>
2016-09-29 12:45:17 +0400

    PL-7910 ORM loads from DB all fields for embedded entity even if partial view is used

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/queries/ObjectLevelReadQuery.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/queries/ObjectLevelReadQuery.java
@@ -1676,6 +1676,9 @@ public abstract class ObjectLevelReadQuery extends ObjectBuildingQuery {
                         fetchedFields.addAll(mappingInAggregate.getFields());
                     }
                 }
+                else {
+                    fetchedFields.addAll(mapping.getFields());
+                }
             } else {
                 fetchedFields.addAll(mapping.getFields());
             }
