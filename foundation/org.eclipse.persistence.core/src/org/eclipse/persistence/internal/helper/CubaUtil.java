ce210d3d5855a4aaaa0c241586e2f3155c67d687
Andrey Subbotin <subbotin@haulmont.com>
2016-11-03 17:46:45 +0400

    PL-8139 Soft Deleted objects are loaded deeply in view

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/helper/CubaUtil.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/internal/helper/CubaUtil.java
@@ -3,6 +3,7 @@ package org.eclipse.persistence.internal.helper;
 public class CubaUtil {
 
     private static ThreadLocal<Boolean> disableSoftDeleteTL = new ThreadLocal<>();
+    private static ThreadLocal<Boolean> disableOriginalSoftDeleteTL = new ThreadLocal<>();
 
     public static boolean setSoftDeletion(boolean softDeletion) {
         Boolean previous = disableSoftDeleteTL.get();
@@ -12,5 +13,15 @@ public class CubaUtil {
 
     public static boolean isSoftDeletion() {
         return !Boolean.TRUE.equals(disableSoftDeleteTL.get());
+    }
+
+    public static boolean setOriginalSoftDeletion(boolean softDeletion) {
+        Boolean previous = disableOriginalSoftDeleteTL.get();
+        disableOriginalSoftDeleteTL.set(!softDeletion);
+        return !Boolean.TRUE.equals(previous);
+    }
+
+    public static boolean isOriginalSoftDeletion() {
+        return !Boolean.TRUE.equals(disableOriginalSoftDeleteTL.get());
     }
 }
