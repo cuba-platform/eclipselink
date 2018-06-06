2b435ee60b51a24631f40d73f7042e5eec6b022a
Andrey Subbotin <subbotin@haulmont.com>
2018-06-06 10:11:49 +0400

    PL-7942 Creating embedded entities in constructor can lead to fetch group bugs

--- foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/mappings/AggregateObjectMapping.java
+++ foundation/org.eclipse.persistence.core/src/org/eclipse/persistence/mappings/AggregateObjectMapping.java
@@ -447,8 +447,8 @@ public class AggregateObjectMapping extends AggregateMapping implements Relation
         } else {
             descriptor.getObjectBuilder().buildAttributesIntoObject(aggregate, buildWrapperCacheKeyForAggregate(cacheKey, targetIsProtected), databaseRow, nestedQuery, joinManager, nestedQuery.getExecutionFetchGroup(descriptor), refreshing, executionSession);
         }
-        if (targetFetchGroup != null && descriptor.hasFetchGroupManager() && cacheKey != null
-                && (!refreshing || ((FetchGroupTracker)aggregate)._persistence_getFetchGroup() == null)
+        if (targetFetchGroup != null && descriptor.hasFetchGroupManager()
+                && (!refreshing && cacheKey != null || ((FetchGroupTracker)aggregate)._persistence_getFetchGroup() == null)
                 && sourceQuery.shouldMaintainCache() && !sourceQuery.shouldStoreBypassCache()) {
             // Set the fetch group to the domain object, after built.
             EntityFetchGroup entityFetchGroup = descriptor.getFetchGroupManager().getEntityFetchGroup(targetFetchGroup);
